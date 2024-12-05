52a41e32bdb465884210be18d039dc3aa6bac78f
garland-0 <garlandhamisi@gmail.com>
2024-12-05 07:00:39 -0800

    commit

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,8 +3,10 @@ import React from 'react';
 const Skills = () => {
     return (
         <div>
-            <h2>My Skills</h2>
-            <p>List of skills goes here.</p>
+            <h2>Skills</h2>
+            <p>languages
+                
+            </p>
         </div>
     );
 };
